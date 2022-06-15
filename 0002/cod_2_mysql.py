670489654794175246f986d9d0161bca5909e5fa
wangyu <1220163813@qq.com>
2022-06-15 11:18:20 +0800

    .

--- 0002/cod_2_mysql.py
+++ 0002/cod_2_mysql.py
@@ -1,4 +1,16 @@
 import pymysql
 
+conn = pymysql.connect(host='localhost', user='root', password='123123123', port=3306, database='mysql')
+cursor = conn.cursor()
 
-conn = pymysql.connect('')
+
+with open('/Users/wangyu/Desktop/cod.txt', 'r') as cod_file:
+
+    for i in cod_file:
+        sql2 = f"insert into mycods values ('{str(i)}')"
+        cursor.execute(sql2)
+
+
+cursor.close()
+conn.commit()
+conn.close()
